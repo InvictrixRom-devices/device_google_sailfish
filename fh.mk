854ceffe539815572a39fc193734aa5584f94701
neporood <neporood@live.com>
2018-02-04 15:30:30 -0600

    Fire It Up
    Change-Id: I827ddb9528dfced7e36fbf13198150c7a9e3be07

--- fh.mk
+++ fh.mk
@@ -2,8 +2,8 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common FireHound stuff.
+$(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
@@ -11,8 +11,8 @@ $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 -include device/google/marlin/sailfish/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_sailfish
-PRODUCT_BRAND := google
+PRODUCT_NAME := fh_sailfish
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel
 TARGET_MANUFACTURER := HTC
 PRODUCT_RESTRICT_VENDOR_FILES := false
