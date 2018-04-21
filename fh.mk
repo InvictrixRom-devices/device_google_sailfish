f953ef0a67239e187fdcaa7f102819d4aba706c6
Curtis G <neporood@users.noreply.github.com>
2018-04-21 13:06:29 -0500

    Invictrify: Sharpen the blade
    Also updated the fingerprint =P

--- fh.mk
+++ fh.mk
@@ -2,21 +2,17 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common FireHound stuff.
-$(call inherit-product, vendor/fh/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/invictrix/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 $(call inherit-product, vendor/nepo/my-apps.mk)
 
-# Disable FH Prebuilts
-DISABLE_CNM := true
-DISABLE_CWB := true
-
 -include device/google/marlin/sailfish/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := fh_sailfish
+PRODUCT_NAME := invictrix_sailfish
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel
 TARGET_MANUFACTURER := HTC
@@ -24,7 +20,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:8.1.0/OPM1.171019.021/4565141:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM1.171019.021 4565141 release-keys"
+    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM2.171019.029 4657601 release-keys"
 
+BUILD_FINGERPRINT=google/sailfish/sailfish:8.1.0/OPM2.171019.029/4657601:user/release-keys
+    
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
